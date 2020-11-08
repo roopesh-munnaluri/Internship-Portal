942dbb28304f36fa0eb274597cffe4a4e083547d
Roopesh <roopesh.munnaluri1@gmail.com>
2020-11-08 17:14:15 -0500

    Updated Database from sqlite3 to postgres

--- intern_data/intern_data/settings.py
+++ intern_data/intern_data/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,10 +77,10 @@ WSGI_APPLICATION = 'intern_data.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": env.db(
+        "DATABASE_URL",
+        default="postgres:///intern_data",
+    )
 }
 
 
