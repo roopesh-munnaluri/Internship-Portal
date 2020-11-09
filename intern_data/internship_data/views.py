b816e900c5a3cdc183eeb60a7e6018e7dacd96f1
Roopesh <roopesh.munnaluri1@gmail.com>
2020-11-08 22:38:58 -0500

    created internship_data application

---
+++ intern_data/internship_data/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+class HomepageView(TemplateView):
+    template_name = 'index.html'
+# Create your views here.
