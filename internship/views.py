2c38de97f5eb007d9f00269c45d3e638224852ef
Roopesh <roopesh.munnaluri1@gmail.com>
2020-11-10 22:33:48 -0500

    Trying heroku deployment

--- internship/views.py
+++ internship/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomepageView(TemplateView):
+    template_name = 'index.html'
+
 
 # Create your views here.
