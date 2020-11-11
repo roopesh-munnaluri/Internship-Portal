8078854076837dd1eb81fa87356a8c3453674276
Roopesh <roopesh.munnaluri1@gmail.com>
2020-11-11 11:17:00 -0500

    updated admin.py to register models into superuser

--- internship/admin.py
+++ internship/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Student, Internship, Internship_Assignment
+
+"""
+Names of tables in admin page
+"""
+admin.site.register(Student)
+admin.site.register(Internship_Assignment)
+admin.site.register(Internship)
+
+
 # Register your models here.
