3b2249bdfc5e2faf44a7af9a7740b94f66a64cd4
Roopesh <roopesh.munnaluri1@gmail.com>
2020-11-10 22:33:48 -0500

    Trying heroku deployment

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from internship.views import HomepageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomepageView.as_view(), name='home'),
 ]
