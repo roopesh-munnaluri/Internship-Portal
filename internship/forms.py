cb28439e0a37fab27b66fb0dd6045c87fe1f9f23
Roopesh <roopesh.munnaluri1@gmail.com>
2020-11-23 17:41:34 -0500

    fixing pylint

--- internship/forms.py
+++ internship/forms.py
@@ -1,8 +1,20 @@
+"""
+forms.py
+author: Mukesh
+Date: 11/22/2020
+"""
 from django import forms
 from .models import Student
 
 
-class StudentSearchForm(forms.ModelForm):
-   class Meta:
-     model = Student
-     fields = ['first_name', 'last_name']
+class StudentSearchForm(forms.ModelForm): # pylint: disable=too-few-public-methods
+    """
+    Used for searching Student Data
+
+    """
+    class Meta: # pylint: disable=too-few-public-methods
+        """
+        For including fields for searching
+        """
+        model = Student
+        fields = ['first_name', 'last_name']
