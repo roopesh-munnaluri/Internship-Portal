02d7b65bec60a214ee592980cb2732ad1eb4a2ff
Roopesh <roopesh.munnaluri1@gmail.com>
2020-11-30 21:38:11 -0500

    added urls for login, logout and register

--- internship/urls.py
+++ internship/urls.py
@@ -1,9 +1,9 @@
 """
 urls.py
 """
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from . import views
-from internship.views import FileuploadView, HomepageView, StudentListView,InternshipListView,InternshipassignmentListView, remove_all_data
+from internship.views import *
 
 
 urlpatterns = [
@@ -13,4 +13,8 @@ urlpatterns = [
         path('internship_list/', InternshipListView.display_internship, name='display_internship' ),
         path('internshipassignment_list/', InternshipassignmentListView.display_internshipassignment, name='display_internshipassignment' ),
         path('remove-data/', remove_all_data, name='remove-data' ),
+        path('login/',Authentication.login_request,name = 'login'),
+        path('logout/',Authentication.logout_request,name='logout'),
+        path('request/',Authentication.register_request,name = 'register')
+
     ]
