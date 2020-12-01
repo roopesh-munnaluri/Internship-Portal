7e8296aa8253016557c551987e0bf90d19b7fd9f
Roopesh <roopesh.munnaluri1@gmail.com>
2020-11-30 21:37:48 -0500

    added forms for login, logout and register forms

--- internship/forms.py
+++ internship/forms.py
@@ -4,9 +4,12 @@ author: Mukesh
 Date: 11/22/2020
 """
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Student # pylint: disable=relative-beyond-top-level
 from .models import Internship
 from .models import Internship_Assignment
+
 class StudentSearchForm(forms.ModelForm): # pylint: disable=too-few-public-methods
     """
     Used for searching Student Data
@@ -43,3 +46,17 @@ class InternshipassignmentSearchForm(forms.ModelForm): # pylint: disable=too-few
         """
         model = Internship_Assignment
         fields = ['year']
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
